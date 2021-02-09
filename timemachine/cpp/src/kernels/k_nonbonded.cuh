b8b88bbd398e53b59c60e35cde48703ee9474727
Yutong Zhao <proteneer@gmail.com>
2021-02-08 20:29:34 -0500

    Fix real cutoff

--- timemachine/cpp/src/kernels/k_nonbonded.cuh
+++ timemachine/cpp/src/kernels/k_nonbonded.cuh
@@ -325,7 +325,7 @@ void __global__ k_nonbonded_du_dx(
 
     RealType cutoff_squared = cutoff*cutoff;
 
-    RealType cutoff = static_cast<RealType>(cutoff);
+    RealType real_cutoff = static_cast<RealType>(cutoff);
 
     RealType real_lambda = static_cast<RealType>(lambda);
     RealType real_beta = static_cast<RealType>(beta);
